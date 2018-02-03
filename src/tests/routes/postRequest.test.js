6bf185f853274e1ab9f865079715bd380b9e2d82
Anmol varma <anmol_varma@github.com>
2018-02-03 20:22:41 +0530

    test file added for get and post responses using jest

---
+++ src/tests/routes/postRequest.test.js
@@ -0,0 +1,36 @@
+const Server = require('../../server');
+
+describe('Server test', () => {
+  const options = {
+    method: 'GET',
+    url: '/route',
+  };
+
+  beforeAll((done) => {
+    Server.on('start', () => {
+      done();
+    });
+  });
+
+  afterAll((done) => {
+    Server.on('stop', () => {
+      done();
+    });
+    Server.stop();
+  });
+
+  test('responds with success statusCode', (done) => {
+    Server.inject(options, (response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+
+  test('response checked', (done) => {
+    const output = 'This is a post request to /route';
+    Server.inject(options, (response) => {
+      expect(response.result).toBe(output);
+      done();
+    });
+  });
+});
