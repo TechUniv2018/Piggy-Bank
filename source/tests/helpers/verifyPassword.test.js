816cc889092a9d618376450b3739cc13d632eee7
surabhi8 <surabhigupta2442@gmail.com>
2018-02-10 16:00:18 +0530

    Test case for verifying password and incorrect digest

--- source/tests/helpers/verifyPassword.test.js
+++ source/tests/helpers/verifyPassword.test.js
@@ -2,11 +2,18 @@ const generateHash = require('../../helpers/generateHash');
 const verifypassword = require('../../helpers/verifyPassword');
 const bcrypt = require('bcrypt');
 
-describe('Testing validation helper function', () => {
-  test('Validation failed for userName less than 5 characters', () => {
+describe('Testing verifyPassword helper function', () => {
+  test('Test case passed for comparing password and its digest', () => {
     bcrypt.hash('Hello1%##', 10).then((hash) => {
       verifypassword('Hello1%##', hash).then((res) => {
         expect(res).toBe(true);
+      });
+    });
+  });
+  test('Test case failed for comparing password and some other string digest', () => {
+    bcrypt.hash('Hello1', 10).then((hash) => {
+      verifypassword('Hello1%##', hash).then((res) => {
+        expect(res).toBe(false);
       });
     });
   });
