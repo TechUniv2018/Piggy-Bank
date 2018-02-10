31401c4d1bed867656c1b67d0ad81261ccaf8dc6
Anmol varma <anmol_varma@github.com>
2018-02-10 16:01:52 +0530

    Changes in the date format and other possible solutions

--- source/routes/getDashboard.js
+++ source/routes/getDashboard.js
@@ -1,4 +1,5 @@
 const Models = require('../../models');
+const strftime = require('strftime');
 
 module.exports = [
   {
@@ -7,17 +8,24 @@ module.exports = [
     handler: (request, response) => {
       Models.users.findOne({ where: { user_name: request.query.username } })
         .then((result) => {
-          const resultObject = {
-            firstName: result.user_firstname,
-            lastName: result.user_lastname,
-            address: result.user_address,
-            phone: result.user_phone,
-            email: result.user_email,
-            dob: result.user_dob,
-            gender: result.user_gender,
-            pan: result.user_pan,
-            img_src: result.user_pic,
-          };
+          let resultObject;
+          if (result === null) {
+            resultObject = { message: 'No such user exsists' };
+          } else {
+            const dob = strftime('%F', new Date(result.user_dob));
+            resultObject = {
+              firstName: result.user_firstname,
+              lastName: result.user_lastname,
+              address: result.user_address,
+              phone: result.user_phone,
+              email: result.user_email,
+              // dob: `${dob.getDate()}-${dob.getMonth() + 1}-${dob.getYear()}`,
+              dob: `(YYYY/MM/DD) ${dob}`,
+              gender: result.user_gender,
+              pan: result.user_pan,
+              img_src: result.user_pic,
+            };
+          }
           response.view('dashboard', resultObject);
         }).catch((err) => {
           response(`${err.message}`);
