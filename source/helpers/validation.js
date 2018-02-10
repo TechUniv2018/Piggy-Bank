63870a4289961cbe765236cb25b177997aabecb7
surabhi8 <surabhigupta2442@gmail.com>
2018-02-10 17:41:25 +0530

    Password regex expression updated and corresponding test cases

--- source/helpers/validation.js
+++ source/helpers/validation.js
@@ -1,14 +1,14 @@
 const Joi = require('joi');
 
 const schema = {
-  userName: Joi.string().min(5).max(15).regex(/^[a-z][a-z0-9_]*$/i),
+  userName: Joi.string().min(5).max(15).regex(/^[a-z][a-zA-Z0-9_]*$/i),
   /*
     password rules :
     1.Start with alphabet
-    2.Contains atleast one small letter, one capital,one special character and one digit
-    3.Can contain any word character
+    2.Contains atleast one digit, one capital,one special character [Order should be same]
+    3.Contain any word character
   */
-  password: Joi.string().min(6).max(20).regex(/^[a-zA-Z](?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])\w$/i),
+  password: Joi.string().min(6).max(20).regex(/^[a-zA-Z][a-zA-Z0-9_]*[0-9][a-zA-Z0-9_]*[A-Z][a-zA-Z0-9]*[%$^&*#@][a-zA-Z0-9_]*$/),
 };
 
 const validation = (data, config) => {
