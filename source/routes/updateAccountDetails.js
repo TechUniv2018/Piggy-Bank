3c1fdc31f2e425fadac10d35ca003024bc5dcb30
paridhi <paridhi815@gmail.com>
2018-02-12 19:10:54 +0530

    Made the update API Generic

--- source/routes/updateAccountDetails.js
+++ source/routes/updateAccountDetails.js
@@ -5,22 +5,16 @@ module.exports = [
     method: 'PUT',
     path: '/users/update',
     handler: (request, response) => {
-      console.log('', request.payload);
+      const updateObject = request.payload;
       const username = request.payload.userName;
-      const value = request.payload.email;
-      // const email = request.payload.email;
-      // const fName = request.payload.fName;
-      // const lName = request.payload.lName;
-      // [{}, {}, {}];
-      Models.bankusers.update({
-        email: value,
-      }, {
+      delete updateObject.userName;
+      Models.bankusers.update(updateObject, {
         where: {
           userName: username,
         },
-      }).then(() => {
+      }).then((result) => {
+        console.log(result);
         // response('Details Changed').code(201);
-        console.log('Inside');
         response({
           statusCode: 201,
           message: 'Details Changed',
