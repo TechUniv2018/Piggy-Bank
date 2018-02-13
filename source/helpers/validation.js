f3e809959f7325563c9ade9e4a80e5cec1593a7c
surabhi8 <surabhigupta2442@gmail.com>
2018-02-13 15:19:21 +0530

    Test cases updated for login request

--- source/helpers/validation.js
+++ source/helpers/validation.js
@@ -1,14 +1,14 @@
 const Joi = require('joi');
 
 const schema = {
-  userName: Joi.string().min(5).max(15).regex(/^[a-z][a-zA-Z0-9_]*$/i),
+  username: Joi.string().min(5).max(15).regex(/^[a-z][a-zA-Z0-9_]*$/i),
   /*
     password rules :
     1.Start with alphabet
     2.Contains atleast one digit, one capital,one special character [Order should be same]
     3.Contain any word character
   */
-  password: Joi.string().min(6).max(20).regex(/^[a-zA-Z][a-zA-Z0-9_]*[0-9][a-zA-Z0-9_]*[A-Z][a-zA-Z0-9]*[%$^&*#@][a-zA-Z0-9_]*$/),
+  userPassword: Joi.string().min(6).max(20).regex(/^[a-zA-Z][a-zA-Z0-9_]*[0-9][a-zA-Z0-9_]*[A-Z][a-zA-Z0-9]*[%$^&*#@][a-zA-Z0-9_]*$/),
 };
 
 const validation = (data, config) => {
