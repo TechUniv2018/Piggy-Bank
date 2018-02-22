14df2d4c80059c9edfc6bae93de8b2c7f770b66b
paridhi <paridhi815@gmail.com>
2018-02-22 17:30:52 +0530

    Test Added For Get request-200 Status code for miniStatement API

---
+++ source/tests/routes/miniStatement.test.js
@@ -0,0 +1,38 @@
+const Server = require('../../server');
+const Models = require('../../../models');
+
+describe('Testing the hapi server for GET request', () => {
+  beforeEach((done) => {
+    Models.transactions.create({
+      transactionId: 'transaction_0081500',
+      transactionStatus: 'complete', // wearebest2D%
+      transactionTimestamp: new Date(),
+      fromAccount: 90351910,
+      toAccount: 90351910,
+      amount: 10000,
+      transactionType: 'credit',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }).then(() => {
+      done();
+    }).catch();
+  });
+  afterEach((done) => {
+    Models.transactions.destroy({
+      where: { transactionId: 'transaction_0081500' },
+      truncate: true,
+    }).then(() => {
+      done();
+    }).catch();
+  });
+  test('Should return 200 status code for sucessful GET request', (done) => {
+    const options = {
+      method: 'GET',
+      url: '/user/miniStatement',
+    };
+    Server.inject(options, (response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+});
