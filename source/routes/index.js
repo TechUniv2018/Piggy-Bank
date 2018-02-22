d8b11e7df7773ee4c562a9d0c3a44a4cce1335dc
paridhi <paridhi815@gmail.com>
2018-02-22 17:08:58 +0530

    Transactions table created and seeder data added.

--- source/routes/index.js
+++ source/routes/index.js
@@ -2,8 +2,9 @@ const moveToDashbboard = require('./getDashboard');
 const loginRequest = require('./loginRequest');
 const updateAccountDetails = require('./updateAccountDetails');
 const editPassword = require('./editPassword');
+const miniStatement = require('./miniStatement');
 
 module.exports = [].concat(
   loginRequest, updateAccountDetails,
-  editPassword, moveToDashbboard,
+  editPassword, moveToDashbboard, miniStatement,
 );
