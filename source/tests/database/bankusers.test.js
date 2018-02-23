146dccc80ce8d36faddc8763bc17e1eaaf0aab15
paridhi <paridhi815@gmail.com>
2018-02-23 10:33:00 +0530

    Checking if insertion is successful in bankusers table

--- source/tests/database/bankusers.test.js
+++ source/tests/database/bankusers.test.js
@@ -10,6 +10,45 @@ describe('test bankusers table', () => {
       done();
     });
   });
+  test('insert transaction into transaction table should be successful', (done) => {
+    const date = new Date();
+    const userObject = {
+      userName: 'pari',
+      password: '$2a$10$uBqWq2mNznlnCisaC.i3UOahjcC9I4CYWy3gGr2w5/oCGCVur0wOm', // wearebest2D%
+      firstName: 'Paridhi',
+      lastName: 'Mohindra',
+      phoneNumber: '9481867815',
+      email: 'pari815@gmail.com',
+      dob: date,
+      gender: 'Female',
+      panCardNumber: 'ABCDE1234F',
+      user_pic: '',
+      fatherName: 'R.K.Mohindra',
+      address: 'where is the house',
+      aadharNumber: '123412341234',
+    };
+
+    Models.bankusers.create(userObject).then((resultUser) => {
+      const userResultObject = {
+        userName: resultUser.dataValues.userName,
+        password: resultUser.dataValues.password,
+        firstName: resultUser.dataValues.firstName,
+        lastName: resultUser.dataValues.lastName,
+        phoneNumber: resultUser.dataValues.phoneNumber,
+        email: resultUser.dataValues.email,
+        dob: resultUser.dataValues.dob,
+        gender: resultUser.dataValues.gender,
+        panCardNumber: resultUser.dataValues.panCardNumber,
+        user_pic: resultUser.dataValues.user_pic,
+        fatherName: resultUser.dataValues.fatherName,
+        address: resultUser.dataValues.address,
+        aadharNumber: resultUser.dataValues.aadharNumber,
+      };
+
+      expect(userResultObject).toEqual(userObject);
+      done();
+    });
+  });
 });
 
 beforeEach((done) => {
