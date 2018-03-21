3e38a0d6de43ddad3f2152533e88364b44bc4ba6
surabhi8 <surabhigupta2442.com>
2018-03-21 14:50:30 +0530

    ministatement api refactored

--- src/routes/miniStatement.js
+++ src/routes/miniStatement.js
@@ -7,7 +7,7 @@ const headerValidation = require('../validations/header');
 
 module.exports = [
   {
-    method: 'GET',
+    method: 'POST',
     path: '/user/miniStatement',
     config: {
       auth: 'jwt',
@@ -20,14 +20,18 @@ module.exports = [
     },
     handler: (request, response) => {
       const { userid } = request.auth.credentials;
-      console.log(userid);
+      const { transactionsCount } = request.payload;
+      console.log(userid, transactionsCount);
       Models.transactions.findAll({
         where: {
           [Op.or]: [{ fromAccount: userid }, { toAccount: userid }],
           transactionStatus: 'complete',
         },
+        order: [
+          ['id', 'DESC'],
+        ],
         attributes: ['toAccount', 'fromAccount', 'fromRemainingBalance', 'toRemainigBalance', 'amount', 'transactionTimestamp'],
-        limit: 10,
+        limit: transactionsCount,
       }).then((result) => {
         if (result === null) {
           const resultObject = { message: 'No transactions' };
