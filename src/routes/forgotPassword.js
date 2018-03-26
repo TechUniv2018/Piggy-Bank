ca82e980745f614e2ecdb0c1a96bea2b1753f9eb
paridhi <paridhi815@gmail.com>
2018-03-26 21:43:16 +0530

    Forgor Password Api Ready and Working

--- src/routes/forgotPassword.js
+++ src/routes/forgotPassword.js
@@ -1,42 +1,38 @@
 
-
-const headerValidation = require('../validations/header');
 const mailer = require('nodemailer');
 const uuid = require('uuid/v1');
-// const html = require('../views/html/forgotPassword.html');
-
+const Models = require('../../models');
 
 const forgotPassword = (request, response) => {
   const userName = request.payload.username;
   const transporter = mailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'pari815@gmail.com',
-      pass: 'paramparapari',
+      user: 'digiBank12345@gmail.com',
+      pass: 'piggybankMckinsey',
     },
   });
-  const token = uuid().substr(0, 10);
-  const url = `http://localhost:3000/auth/reset_password?token=${token}`;
+  const token = uuid();
+  const url = `http://localhost:3000/#/resetPassword/${token}`;
   const mailOptions = {
-    from: 'pari815@gmail.com',
+    from: 'digiBank@gmail.com',
     to: 'paridhi815@gmail.com',
     subject: 'Paridhi, heres the link to reset your password',
     text: 'Reset Your PassWord!',
-    // // html,
-    // html: `<b>Hello ${userName}, </b><br> To change your DigiBank password, click here or paste the following link into your browser:<br> url <br> This link will expire in 24 hours, so be sure to use it right away.<br>Thank you for using DigiBank!<br>The DigiBank Team :)`,
-    // context: {
-    //   url: `http://localhost:3000/auth/reset_password?token=${token}`,
-    //   //   name: user.fullName.split(' ')[0],
-    //   name: 'paridhi',
-    // },
-
     html: `<b>Hello ${userName}, </b><br> To change your DigiBank password, click here or paste the following link into your browser:<br> ${url} <br> This link will expire in 1 hour, so be sure to use it right away.<br>Thank you for using DigiBank!<br>The DigiBank Team :)`,
-    resetUrl: 'http;//localhost:3000/password_rest/000000000001|afdaevdae353',
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
+    console.log(userName, token);
+    Models.bankusers.update({
+      resetCode: token,
+    }, {
+      where: {
+        userName,
+      },
+    });
     if (error) {
-      response(error);
+      response(error.message);
     } else {
       response(`Email sent: ${info.response}`);
     }
@@ -46,15 +42,12 @@ const forgotPassword = (request, response) => {
 module.exports = [
   {
     method: 'POST',
-    path: '/resetPassword',
+    path: '/forgot/password',
     config: {
-      auth: 'jwt',
+      auth: false,
       tags: ['api'],
       description: 'password reset api',
       notes: 'password reset api',
-      validate: {
-        headers: headerValidation,
-      },
     },
     handler: forgotPassword,
   }];
